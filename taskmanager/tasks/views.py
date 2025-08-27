4c4bcc33b59673f3b8fff5e62c9aec2064ef1f4d
khansae <khansaeugc18@gmail.com>
2025-08-27 20:34:50 +0200

    final

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,37 +1,41 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
-from django.contrib.auth.decorators import login_required
 
-@login_required
-def update_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id, user=request.user)
+# List all tasks
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+# Add a new task
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+# Edit an existing task
+def task_edit(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('tasks_list')
+            return redirect('task_list')
     else:
         form = TaskForm(instance=task)
-    return render(request, 'tasks/update_task.html', {'form': form})
-@login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id, user=request.user)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+# Delete a task
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
         task.delete()
-        return redirect('tasks_list')
-    return render(request, 'tasks/delete_task.html', {'task': task})
-@login_required
-def create_task(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            new_task = form.save(commit=False)
-            new_task.user = request.user  # Link task to logged-in user
-            new_task.save()
-            return redirect('tasks_list')
-    else:
-        form = TaskForm()
-    return render(request, 'tasks/create_task.html', {'form': form})
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
 
 
